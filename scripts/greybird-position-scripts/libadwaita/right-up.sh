af007eb2547acc5fade47f53067834cbc0c4ef60
Sean Davis <sean@bluesabre.org>
2022-09-03 08:39:45 -0400

    Add screenshot positioning scripts for Greybird (SSD, CSD, libadwaita)

---
+++ scripts/greybird-position-scripts/libadwaita/right-up.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+IFS='x' read screenWidth screenHeight < <(xdpyinfo | grep dimensions | grep -o '[0-9x]*' | head -n1)
+
+width=$(xdotool getactivewindow getwindowgeometry --shell | head -4 | tail -1 | sed 's/[^0-9]*//')
+height=$(xdotool getactivewindow getwindowgeometry --shell | head -5 | tail -1 | sed 's/[^0-9]*//')
+
+newPosX=$((screenWidth-width))
+newPosY=0
+
+xdotool getactivewindow windowmove "$newPosX" "$newPosY"
---
+++ scripts/greybird-position-scripts/ssd/left-down.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+IFS='x' read screenWidth screenHeight < <(xdpyinfo | grep dimensions | grep -o '[0-9x]*' | head -n1)
+
+width=$(xdotool getactivewindow getwindowgeometry --shell | head -4 | tail -1 | sed 's/[^0-9]*//')
+height=$(xdotool getactivewindow getwindowgeometry --shell | head -5 | tail -1 | sed 's/[^0-9]*//')
+
+newPosX=61
+newPosY=$((screenHeight-height-70))
+
+xdotool getactivewindow windowmove "$newPosX" "$newPosY"
